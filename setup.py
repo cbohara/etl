3020205f098fe51e7fd464d1e5abe59ebbec609d
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-11-05 20:12:47 -0800

    setting up testing framework

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('/Users/charlieohara/Documents/Egnyte/Private/charlie.ohara/code/github/cbohara/etl/requirements.txt') as fd:
+    requirements = [line.rstrip() for line in fd]
+
 setup(
-    name='etl'
+    name='etl',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    install_requires=requirements,
 )
